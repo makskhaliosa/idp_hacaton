7c11570da288626353d77496eae1284bb9049215
Andrey Varachev <Dartanyun@yandex.com>
2024-01-17 00:35:59 +0500

    Created new app - idp_app
    This application will be home to the basic data tables and their settings.
    
    Current Databases - Task, Department, Company, File

---
+++ idp_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IdpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'idp_app'
---
+++ idp_app/models.py
@@ -0,0 +1,168 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class TaskStatuses(models.TextChoices):
+    """
+    Status table for tasks.
+    """
+
+    OPEN = 'Open',
+    IN_PROGRESS = 'In Progress',
+    NEED_DETAILS = 'Need Details',
+    REASSIGNED = 'Reassigned',
+    REVIEW = 'Review',
+    HOLD = 'Hold',
+    CLOSED = 'Closed'
+ 
+
+class Company(models.Model):
+    """
+    Company table.
+    """
+    company_id = models.IntegerField(
+        primary_key=True,
+        verbose_name='company_id',
+    )
+    company_name = models.CharField(
+        verbose_name='company_name',
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ('company_id',)
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
+    def __str__(self) -> str:
+        return self.company_name
+
+class Department(models.Model):
+    """
+    Department table.
+    """
+    dep_id = models.IntegerField(
+        primary_key=True,
+        verbose_name='dep_id'
+    )
+    dep_name = models.CharField(
+        verbose_name='dep_name',
+        max_length=400
+    )
+    company_id = models.ForeignKey(
+        Company, on_delete=models.CASCADE,
+        verbose_name='company_id',
+    )
+    class Meta:
+        ordering = ('dep_id',)
+        verbose_name = 'Department'
+        verbose_name_plural = 'Departmens'
+
+    def __str__(self) -> str:
+        return self.dep_name
+
+class Task(models.Model):
+    """
+    Tasks table.
+    """
+    task_id = models.IntegerField(
+        primary_key=True,
+        verbose_name='task_id'
+    )
+    task_name = models.CharField(
+        verbose_name='task_name',
+        blank=False,
+        max_length=150
+    )
+    task_description = models.CharField(
+        verbose_name='task_description',
+        blank=False,
+        max_length=10000,
+    )
+    task_status = models.CharField(
+        verbose_name='task_status',
+        max_length=40,
+        choices=TaskStatuses.choices,
+        default=TaskStatuses.OPEN,
+        blank=False,
+    )
+    task_start_date = models.DateTimeField(
+        verbose_name='task_start_date',
+        auto_now_add=True,
+    )
+    task_end_date_plan = models.DateTimeField(
+        verbose_name='task_end_date_plan',
+        blank=False,
+    )
+    task_end_date_fact = models.DateTimeField(
+        verbose_name='task_end_date_plan',
+        blank=True
+    )
+    task_note_employee = models.CharField(
+        verbose_name='task_note_employee',
+        max_length=10000,
+        blank=False,
+    )
+    task_note_cheif = models.CharField(
+        verbose_name='task_note_cheif',
+        max_length=10000,
+        blank=False,
+    )
+    task_note_mentor = models.CharField(
+        verbose_name='task_note_mentor',
+        max_length=10000,
+        blank=False
+    )
+    task_mentor_id = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name='task_mentor_id',
+        related_name='mentor_tasks',
+        null=True
+    )
+
+    class Meta:
+        ordering = ('task_id',)
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self) -> str:
+        return f'Task №{self.task_id}'
+
+
+class File(models.Model):
+    """
+    Files table.
+    """
+    file_id = models.IntegerField(
+        primary_key=True,
+        verbose_name='file_id'
+    )
+    file_name = models.CharField(
+        verbose_name='file_name',
+        blank=False,
+        max_length=100
+    )
+    file_link = models.CharField(
+        verbose_name='file_link',
+        blank=False,
+        max_length=1000
+    )
+    file_type = models.CharField(
+        verbose_name='file_type',
+        blank=False,
+        max_length=10
+    )
+    file_task_id = models.ForeignKey(
+        Task, on_delete=models.CASCADE,
+        verbose_name='file_task_id'
+    )
+
+    class Meta:
+        ordering = ('file_id',)
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
+
+    def __str__(self) -> str:
+        return self.filename
