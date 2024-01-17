f6d1c818ce9ebf6cb360c189fbfb4395465ab2a2
Rashid Ayoupov <valentaine1234@gmail.com>
2024-01-17 17:01:24 +0300

    drf settings

--- idp/settings.py
+++ idp/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     # project`s apps
     'users.apps.UsersConfig',
     'idp_app.apps.IdpAppConfig',
