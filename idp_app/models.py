8b65820084f38713241cbda0bda9f35d661f18f8
Alexey Chizhov <aalexey-chizhov@yandex.ru>
2024-01-17 14:12:01 +0300

    add IDP model
    Добавлена модель ИПР. Посмотрите функцию default_end_date_plan(). Оставлять ее здесь или лучше вынести в другой класс, чтобы не засорять модели

--- idp_app/models.py
+++ idp_app/models.py
@@ -1,3 +1,71 @@
+from datetime import datetime, timedelta
 from django.db import models
+import uuid
 
-# Create your models here.
+from users.models import User
+
+
+def default_end_date_plan():
+    return timezone.now() + timedelta(days=30)
+
+
+class IDP(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Активные'),
+        ('completed', 'Выполненные'),
+        ('two_weeks', 'Две недели до плановой даты выполнения'),
+        ('overdue', 'Просроченные'),
+        ('canceled', 'Отмененные'),
+        ('closed', 'Закрытые'),
+        ('draft', 'Черновик')
+    ]
+
+    idp_id = models.UUIDField(
+        primary_key=True,
+        verbose_name='idp_id',
+        default=uuid.uuid4,
+        editable=False,
+        blank=False,
+    )
+    name = models.CharField(
+        verbose_name='name',
+        max_length=100,
+        blank=False,
+        null=False
+    )
+    target = models.TextField(
+        verbose_name='target',
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    status = models.CharField(
+        verbose_name='status',
+        max_length=255,
+        choices=STATUS_CHOICES,
+        default='draft',
+        blank=False,
+        null=False
+    )
+    start_date = models.DateField(
+        verbose_name='start_date',
+        default=datetime.now,
+        blank=True,
+        null=True
+    )
+    end_date_plan = models.DateField(
+        verbose_name='end_date_plan',
+        default=default_end_date_plan,
+        blank=True,
+        null=True
+    )
+    end_date_fact = models.DateField(
+        verbose_name='end_date_fact',
+        blank=True,
+        null=True
+    )
+    employee_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=False,
+    )
