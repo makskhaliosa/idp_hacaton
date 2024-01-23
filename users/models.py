9c3611b75c85bbd979be7fb734e8fe0bb1bfde86
Максим Халёса <playwright@inbox.ru>
2024-01-23 15:02:38 +0300

    Update user model, now extends AbstractBaseUser, add CustomUserManager, update fixtures. Pre-commit fixes.

--- users/models.py
+++ users/models.py
@@ -1,10 +1,13 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.utils.itercompat import is_iterable
+
+from .managers import CustomUserManager
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     """Таблица для сотрудников."""
 
     uid = models.UUIDField(
@@ -17,8 +20,9 @@ class User(AbstractUser):
     middle_name = models.CharField(
         verbose_name="middle_name", max_length=100, blank=True
     )
-    last_name = models.CharField(
-        verbose_name="last_name", db_index=True, max_length=100
+    last_name = models.CharField(verbose_name="last_name", max_length=100)
+    email = models.EmailField(
+        verbose_name="user_email", max_length=100, db_index=True, unique=True
     )
     position = models.ForeignKey(
         "Position",
@@ -35,14 +39,6 @@ class User(AbstractUser):
         blank=True,
         null=True,
     )
-    mentor = models.ForeignKey(
-        "User",
-        on_delete=models.SET_NULL,
-        verbose_name="mentor",
-        related_name="students",
-        blank=True,
-        null=True,
-    )
     department = models.ForeignKey(
         "Department",
         on_delete=models.SET_NULL,
@@ -51,6 +47,32 @@ class User(AbstractUser):
         null=True,
     )
     is_admin = models.BooleanField(verbose_name="user_is_admin", default=False)
+    is_staff = models.BooleanField(
+        verbose_name="staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        verbose_name="active",
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    is_superuser = models.BooleanField(
+        verbose_name="superuser status",
+        default=False,
+        help_text=(
+            "Designates that this user has all permissions without "
+            "explicitly assigning them."
+        ),
+    )
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -59,6 +81,55 @@ class User(AbstractUser):
         ordering = ("last_name", "first_name")
         verbose_name = "User"
         verbose_name_plural = "Users"
+
+    def get_full_name(self):
+        """
+        Return full name.
+
+        The first_name, middle_name if there is one and the last_name,
+        with a space in between.
+        """
+        middle_full_name = (
+            f"{self.first_name} {self.middle_name} {self.last_name}"
+        )
+        full_name = f"{self.first_name} {self.last_name}"
+        return middle_full_name if self.middle_name else full_name
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def has_perm(self, perm, obj=None):
+        """
+        Return True if the user is active and admin or superuser.
+
+        Needs further development for common users.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and (self.is_admin or self.is_superuser):
+            return True
+        return False
+
+    def has_perms(self, perm_list, obj=None):
+        """
+        Return True if the user has each of the specified permissions.
+
+        If object is passed, check if the user has all required perms for it.
+        """
+        if not is_iterable(perm_list) or isinstance(perm_list, str):
+            raise ValueError("perm_list must be an iterable of permissions.")
+        return all(self.has_perm(perm, obj) for perm in perm_list)
+
+    def has_module_perms(self, app_label):
+        """
+        Return True if the user has any permissions in the given app label.
+
+        Use similar logic as has_perm(), above.
+        """
+        # Active superusers have all permissions.
+        if self.is_active and (self.is_admin or self.is_superuser):
+            return True
+        return False
 
 
 class Position(models.Model):
