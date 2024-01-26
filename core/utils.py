6b3463600cbe6f1bb7c284c94e2c87212707d1d5
Максим Халёса <playwright@inbox.ru>
2024-01-26 13:42:09 +0300

    Update docs in find_differencies.

--- core/utils.py
+++ core/utils.py
@@ -9,10 +9,17 @@ def default_end_date_plan():
 
 
 def find_differencies(initial: Model, updated: Model):
-    """Return differencies between fields of two objects."""
+    """
+    Return differencies between fields of two objects.
+
+    Compares two dicts of original and updated objects.
+    If any field differs, we take the updated value.
+    Return dict with new values.
+    """
     d1 = initial.__dict__
     d2 = updated.__dict__
     diffs = {k: v for k, v in d2.items() if v != d1[k]}
     if "_state" in diffs:
+        # _state нам без надобности, поэтому убираем
         del diffs["_state"]
     return diffs
